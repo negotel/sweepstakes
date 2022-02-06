31a157dfccff48ac07d3407a55ff879d81700916
Edson Costa <edsonrcoosta@gmail.com>
2022-02-06 18:28:04 -0300

    revendo

--- .r
+++
@@ -1,9 +0,0 @@
-try:
-    con_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\edson\Desktop\consultas\consulta.mdb;'
-    conn = pyodbc.connect(con_string)
-    print("Connected To Database")
- 
- 
- 
-except pyodbc.Error as e:
-    print("Error in Connection", e)
